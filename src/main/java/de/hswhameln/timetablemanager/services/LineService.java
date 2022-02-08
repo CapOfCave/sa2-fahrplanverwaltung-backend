2ce9c0d4a81c45c8821cfdd43cc2c4910f2a081a
Lars Kecker <lars.kecker2009@gmail.com>
2022-02-08 17:03:21 +0100

    fix imports

--- src/main/java/de/hswhameln/timetablemanager/services/LineService.java
+++ src/main/java/de/hswhameln/timetablemanager/services/LineService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
+import java.util.Optional;
 
 @Service
 public class LineService {
