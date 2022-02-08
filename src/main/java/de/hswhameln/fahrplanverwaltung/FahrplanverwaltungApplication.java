301dc66c0170c1e662715b2583ea2718d42b0e6e
Lars Kecker <lars.kecker2009@gmail.com>
2022-02-08 11:08:08 +0100

    create initial maven project

---
+++ src/main/java/de/hswhameln/fahrplanverwaltung/FahrplanverwaltungApplication.java
@@ -0,0 +1,13 @@
+package de.hswhameln.fahrplanverwaltung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FahrplanverwaltungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FahrplanverwaltungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/hswhameln/fahrplanverwaltung/FahrplanverwaltungApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hswhameln.fahrplanverwaltung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FahrplanverwaltungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
