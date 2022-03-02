8dab322191ada776d0a35cbd666c04110a857f46
Lars Kecker <lars.kecker2009@gmail.com>
2022-03-02 13:22:57 +0100

    create additional integration tests for use cases 11 and 12

--- src/test/java/de/hswhameln/timetablemanager/controller/BusStopControllerTest.java
+++ src/test/java/de/hswhameln/timetablemanager/controller/BusStopControllerTest.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -20,7 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
- * Integration test for verifying application behavior regarding use cases 2, 6, and 10
+ * Integration test for verifying application behavior regarding use cases 2, 6, 10, and 12
  */
 class BusStopControllerTest extends IntegrationTest {
 
@@ -110,6 +111,35 @@ class BusStopControllerTest extends IntegrationTest {
                         delete("/busstops/{busStopId}/", 8))
                 .andDo(print())
                 .andExpect(status().isOk());
+
+    }
+
+    @Test
+    @Sql("/data-test.sql")
+    void testGetBusStopDetail() throws Exception {
+
+        String expectedResponse = """
+                {
+                  "id": 1,
+                  "name": "Abbey Road",
+                  "lines": [
+                    {
+                      "id": 1,
+                      "name": "1"
+                    },
+                    {
+                      "id": 2,
+                      "name": "S65"
+                    }
+                  ]
+                }            
+                """;
+        this.mockMvc.perform(
+                        get("/busstops/{busStopId}/", 1))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedResponse));
+
 
     }
 
--- src/test/java/de/hswhameln/timetablemanager/controller/LineControllerTest.java
+++ src/test/java/de/hswhameln/timetablemanager/controller/LineControllerTest.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -20,7 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
- * Integration test for verifying application behavior regarding use cases 3, 6, and 9
+ * Integration test for verifying application behavior regarding use cases 3, 6, 9, and 11
  */
 class LineControllerTest extends IntegrationTest {
 
@@ -122,6 +123,48 @@ class LineControllerTest extends IntegrationTest {
                         delete("/lines/{lineId}/", 2))
                 .andDo(print())
                 .andExpect(status().isOk());
+
+    }
+
+    @Test
+    @Sql("/data-test.sql")
+    void testGetLineDetail() throws Exception {
+
+        String expectedResponse = """
+                {
+                   "id": 1,
+                   "name": "1",
+                   "lineStops": [
+                     {
+                       "id": 1,
+                       "index": 0,
+                       "secondsToNextStop": 60,
+                       "busStopId": 1,
+                       "busStopName": "Abbey Road"
+                     },
+                     {
+                       "id": 2,
+                       "index": 2,
+                       "secondsToNextStop": 120,
+                       "busStopId": 4,
+                       "busStopName": "Dean Avenue"
+                     },
+                     {
+                       "id": 3,
+                       "index": 3,
+                       "secondsToNextStop": null,
+                       "busStopId": 5,
+                       "busStopName": "East Hills Avenue"
+                     }
+                   ]
+                 }
+                """;
+        this.mockMvc.perform(
+                        get("/lines/{lineId}/", 1))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedResponse));
+
 
     }
 }
