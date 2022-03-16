31221abe475657735049ed889961fab221849a7c
Lars Kecker <lars.kecker2009@gmail.com>
2022-03-16 11:36:55 +0100

    add schedule controller tests

--- src/test/java/de/hswhameln/timetablemanager/controller/ScheduleControllerTest.java
+++ src/test/java/de/hswhameln/timetablemanager/controller/ScheduleControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -84,13 +85,33 @@ class ScheduleControllerTest extends IntegrationTest {
                  ]
                 """;
 
-        this.mockMvc.perform(
-                        get("/schedules/")
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(requestBody))
+        this.mockMvc.perform(get("/schedules/"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().json(expectedResponseGet));
+    }
+
+    @Test
+    @DisplayName("When trying to create a schedule with an invalid lineId a proper exception is thrown")
+    @Sql("/data-test.sql")
+    void testCreatingScheduleLineNotFound() throws Exception {
+
+        String requestBody = """
+                {
+                   "lineId": 7777,
+                   "startTime": "14:35:00",
+                   "reverseDirection": false
+                }
+                """;
+
+        String expectedResponse = "Line with lineId '7777' was not found. Reason: It does not exist.";
+        this.mockMvc.perform(
+                        post("/schedules/")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(requestBody))
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(content().string(expectedResponse));
     }
 
     @Test
@@ -103,6 +124,78 @@ class ScheduleControllerTest extends IntegrationTest {
                 .andExpect(status().isOk());
 
         assertEquals(0, this.scheduleRepository.count());
+    }
+
+    @Test
+    @DisplayName("When trying to delete a schedule with an invalid scheduleId a proper exception is thrown")
+    @Sql("/data-test.sql")
+    void testDeleteScheduleNotFound() throws Exception {
+        String expectedResponse = "Schedule with scheduleId '7777' was not found. Reason: It does not exist.";
+        this.mockMvc.perform(
+                        delete("/schedules/{scheduleId}", 7777L))
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(content().string(expectedResponse));
+
+        assertEquals(1, this.scheduleRepository.count());
+    }
+
+    @Test
+    @DisplayName("As an employee, I can modify a schedule's direction and start time.")
+    @Sql("/data-test.sql")
+    void testModifySchedule() throws Exception {
+
+        String requestBody = """
+                {
+                   "startTime": "14:38:00",
+                   "reverseDirection": true
+                }
+                """;
+
+        String expectedResponseCreate = """
+                {
+                    "id": 1,
+                    "startTime": "14:38:00",
+                    "line": {
+                      "id": 1,
+                      "name": "1"
+                    },
+                    "finalStop": {
+                      "id": 1,
+                      "name": "Abbey Road"
+                    }
+                }
+                """;
+
+        this.mockMvc.perform(
+                        patch("/schedules/{scheduleId}", 1L)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(requestBody))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedResponseCreate));
+
+    }
+
+    @Test
+    @DisplayName("When trying to modify a schedule with an invalid scheduleId a proper exception is thrown")
+    @Sql("/data-test.sql")
+    void testModifyScheduleNotFound() throws Exception {
+
+        String requestBody = """
+                {
+                   "startTime": "14:38:00",
+                   "reverseDirection": true
+                }
+                """;
+        String expectedResponse = "Schedule with scheduleId '7777' was not found. Reason: It does not exist.";
+        this.mockMvc.perform(
+                        patch("/schedules/{scheduleId}", 7777L)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(requestBody))
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(content().string(expectedResponse));
     }
 
 }
